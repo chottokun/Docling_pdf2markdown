cbb4e8af71cbc9b46fefc231604f2093b559644b
chottokun <cbwgift@gmail.com>
2026-02-23 17:49:53 +0900

    Fix server test imports

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from pathlib import Path
-from src.docling_lib.server import app
+from docling_lib.server import app
 import shutil
 
 client = TestClient(app)
